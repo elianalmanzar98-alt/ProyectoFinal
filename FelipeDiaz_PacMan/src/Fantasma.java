4e24df61a366515a7ca8d9855298c437f31d5a32
elianalmanzar98@gmail.com <elianalmanzar98@gmail.com>
2026-04-02 17:47:05 -0400

    modificaciones y movimiento de fantasmas

--- FelipeDiaz_PacMan/src/Fantasma.java
+++ FelipeDiaz_PacMan/src/Fantasma.java
@@ -1,3 +1,131 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
 public class Fantasma {
     
+
+    private int fila;
+    private int columna;
+    private ImageView fantasmaImag;
+    private int [] ultimoMovimiento = null;
+    private boolean estaAsustado;
+    private int [] posicionInicial;
+
+    public boolean isEstaAsustado(){
+        return estaAsustado;
+    }
+    public void setEstaAsustado(boolean estaAsustado){
+        this.estaAsustado = estaAsustado;
+
+    }
+    public int [] getPosicionInicial(){
+        return posicionInicial;
+    }
+
+    nombreFantasmas nombre;
+    
+
+    public Fantasma(int fila, int columna, ImageView fantasmaImag, nombreFantasmas nombre){
+        this.fila = fila;
+        this.columna =  columna;
+        this.fantasmaImag = fantasmaImag;
+        this.nombre = nombre;
+        this.estaAsustado = false;
+        posicionInicial = new int[]{fila, columna};
+    }
+    public void iniciarMovimiento(Jugador jugador, GridPane tablero, Mapa mapa){
+        int objetivodFila, objetivodColumna;
+        if(nombre == nombreFantasmas.RED){
+            objetivodFila = jugador.getFila() -2;
+            posicionInicial = new int[]{11,12};
+            objetivodColumna = jugador.getColumna() +2;
+        }
+        else{
+            objetivodColumna = jugador.getColumna();
+            objetivodFila = jugador.getFila();
+            posicionInicial = new int[]{11,11};
+        }
+
+        List<int[]> posiblesMovimientos = new ArrayList<>();
+
+        if (mapa.getCelda(fila + 1, columna)!=1){
+            if((fila + 1) != posicionInicial[0] || columna != posicionInicial[1]) posiblesMovimientos.add(new int[]{1,0});
+        }
+        if (mapa.getCelda(fila - 1, columna - 1) !=1){
+            if (fila != posicionInicial[0] || (columna -1) != posicionInicial[1]) posiblesMovimientos.add(new int[]{0,1});
+        }
+
+        if(mapa.getCelda(fila - 1, columna) !=1){
+            if((fila -1) != posicionInicial[0] || columna != posicionInicial[1]) posiblesMovimientos.add(new int[]{-1,0});
+        }
+        if(mapa.getCelda(fila, columna + 1) !=1){
+            if(fila != posicionInicial[0] || (columna + 1)  != posicionInicial[1]) posiblesMovimientos.add(new int[]{0,1});
+        }
+
+        if (!posiblesMovimientos.isEmpty()){
+
+
+            if(ultimoMovimiento != null && posiblesMovimientos.size() > 1){
+                posiblesMovimientos.removeIf(mov -> mov [0]== -ultimoMovimiento[0] && mov [1] == -ultimoMovimiento[1]);
+            }
+            int [] mejorMovimiento = posiblesMovimientos.get(0);
+            double distanciaMayor = Double.MIN_VALUE;
+
+            //aqui quiero buscar el movimiento de mi ppacman mas lejano 
+
+            for (int[] mov : posiblesMovimientos){
+                int nuevaFila = fila + mov[0];
+                int nuevaColumna = columna + mov[1];
+                double distancia = Math.sqrt(
+                    Math.pow(nuevaFila - objetivodFila, 2)+ Math.pow(nuevaColumna - objetivodColumna, 2)
+                );
+                if(distancia > distanciaMayor){
+                    distanciaMayor = distancia;
+                    mejorMovimiento = mov;
+                }
+            }
+            fila += mejorMovimiento[0];
+            columna += mejorMovimiento[1];
+            ultimoMovimiento = mejorMovimiento;
+        }
+
+        tablero.setRowIndex(fantasmaImag, fila);
+        tablero.setColumnIndex(fantasmaImag,columna);
+
+    }
+
+    public void recuperarAsustado(nombreFantasmas n ){
+        if( n == nombreFantasmas.RED)
+        fantasmaImag.setImage(new Image());
+        else fantasmaImag.setImage(new Image());
+
+        fantasmaImag.setVisible(true);
+        this.estaAsustado = false;
+
+    }
+
+    public void setImagen(Image image){
+        fantasmaImag = new ImageView(image);
+    }
+    public ImageView getImagen(){
+        return fantasmaImag;
+    }
+    public void setPosicion(int[] pos){
+        this.fila = pos[0];
+        this.columna = pos[1];
+    }
+    public int getFila(){
+        return fila;
+    }
+    public int getColumna(){
+        return columna;
+    }
+
+
+
+
 }
--- FelipeDiaz_PacMan/src/Jugador.java
+++ FelipeDiaz_PacMan/src/Jugador.java
@@ -1,6 +1,10 @@
 import java.lang.classfile.Label;
 
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
 
 public class Jugador {
 
@@ -17,10 +21,178 @@ public class Jugador {
     private Fantasma BLUE;
     private long milisAsustado;
 
+    public Jugador(int fila, int columna, ImageView imagen){
+        this.fila = fila;
+        this.columna = columna;
+        this.imagen = imagen;
+        puntuacion = 0;
+        vidas = 3;
+        AsustadoActivado = false;
+        PuntosRecolectados = 0;
+    }
+
+    public void mover(KeyCode tecla, GridPane tablero)throws InterruptedException{
+        int nuevaFila = fila;
+        int nuevaColumna = columna;
+
+        switch (tecla) {
+            case UP:
+                nuevaFila--;
+                break;
+            case DOWN:
+                nuevaFila++;
+                break;  
+            case LEFT:
+                nuevaColumna--;
+                break;
+            case RIGHT:
+                nuevaColumna++;
+                break;
+            default:
+                break;
+                
+        }
+        if(mapa.getCelda(nuevaFila, nuevaColumna) != 1 && mapa.getCelda(nuevaFila, nuevaColumna) != 4){
+            rotarImagen(tecla);
+            
+
+            boolean hayFantasmas = (fila == RED.getFila() && columna == RED.getColumna()) || (fila == BLUE.getColumna());
+
+
+            fila = nuevaFila;
+            columna = nuevaColumna;
+
+            if(!hayFantasmas){
+                Node n = obtenerNodoCelda(tablero, nuevaColumna, nuevaFila);
+                if(n != null && n.isVisible()){
+                    if(mapa.getCelda(nuevaFila, nuevaColumna) == 0){
+                        puntuacion+=10;
+                        PuntosRecolectados ++;
+                    }
+                    else if(mapa.getCelda(nuevaFila, nuevaColumna) == 2){
+                        puntuacion += 20;
+                        PuntosRecolectados++;
+                        AsustadoActivado = true;
+                        RED.setEstaAsustado(AsustadoActivado);
+                        BLUE.setEstaAsustado(AsustadoActivado);
+                        milisAsustado = System.currentTimeMillis();
+
+                    }
+                    n.setVisible(false);
+                    Platform.runLater(()->{
+                        puntajeLabel.setText(puntuacion + " ");
+                    });
+
+                }
+
+            }
+            tablero.setRowIndex(imagen, fila);
+            tablero.setColumnIndex(imagen, columna);
+
+        }
+
+    }
+    public Node obtenerNodoCeldaNode(GridPane gridpane, int fila, int columna){
+        for (Node node : gridpane.getChildren()){
+            Integer nodeFila = GridPane.getRowIndex(node);
+            Integer nodeColumna = GridPane.getColumnIndex(node);
+            if (nodeFila == null) nodeFila = 0;
+            if (nodeColumna == null) nodeColumna = 0;
+            if(nodeFila == fila && nodeColumna == columna){
+                return node;
+            } 
+            
+        }
+        return null;
+            }
+        private void rotarImagen(KeyCode tecla){
+            switch (tecla) {
+                case UP:
+                    imagen.setRotate(270);
+
+                    break;
+                case DOWN:
+                    imagen.setRotate(90);
+                    break;
+                case RIGHT:
+                    imagen.setRotate(0);
+                    break;
+                case LEFT:
+                    imagen.setRotate(180);
+                default:
+                    break;
+            }
+            
+            
+        }
+
+        public int getFila(){
+            return fila ;
+        }
+        public int getColumna(){
+            return columna;
+        }
+
+        public void setMapa(Mapa mapa){
+            this.mapa = mapa;
+        }
+        public double getPuntuacion(){
+            return puntuacion;
+        }
+        public ImageView getImageView(){
+            return imagen;
+        }
+        public void setFila(int fila){
+            this.fila = fila;
+        }
+        public void setLabelPuntuacion( Label puntLabel){
+            this.puntajeLabel = puntLabel;
+        }
+        public void setColumna( int columna){
+            this.columna = columna;
+        }
+        public void setImagen(ImageView imagen){
+            this.imagen = imagen;
+        }
+        public Mapa getMapa(){
+            return mapa;
+        }
+        public void setPuntuacion(double puntuacion){
+            this.puntuacion = puntuacion;
+        }
+        public int getVidas(){
+            return vidas;
+        }
+        public void setVidas( int vidas){
+            this.vidas = vidas;
+        }
+        public void setPuntajeLabel(Label puntajelabel){
+            this.puntajeLabel = puntajelabel;
+        }
+        public boolean isAsustadoActivado(){
+            return AsustadoActivado;
+        }
+        public void setAsustadoActivado(boolean AsustadoActivado){
+            this.AsustadoActivado = AsustadoActivado;
+
+        }
+        public void setPosicion(int fila, int columna){
+            this.fila = fila;
+            this.columna = columna;
+        }
+        public void setFantasmas(Fantasma RED, Fantasma BLUE){
+            this.RED = RED;
+            this.BLUE = BLUE;
+        }
+        public long getMilisAsustado(){
+            return milisAsustado;
+        }        
+
+        }
 
 
 
 
     
     
-}
+
